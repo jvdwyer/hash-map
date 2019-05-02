0666509de00678f164bca52349ae649a4461b570
jvdwyer <jvdwyer1994@gmail.com>
2019-05-02 16:10:12 -0500

    first commit

---
+++ TestHashMap.py
@@ -0,0 +1,11 @@
+from HashMap import HashMap
+
+hash_map = HashMap(15)
+
+hash_map.assign("gabbro", "igneaous")
+hash_map.assign("sandstone", "sedimentary")
+hash_map.assign("gneiss", "metamorphic")
+
+print(hash_map.retrieve("gabbro"))
+print(hash_map.retrieve("sandstone"))
+print(hash_map.retrieve("gneiss"))
